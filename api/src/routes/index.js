344d8e0415a9c48e5c020689ab777c7a0fda31bb
NicolasQuirogaa <nicolas.aquiroga@hotmail.com>
2022-02-16 21:23:03 -0300

    back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 
+// Importar todos los routers;
+const dogs = require('./routesDog');
+const temperaments = require('./routesTemperaments');
+const breeds = require('./routesBreeds');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/', dogs);
+router.use('/', temperaments);
+router.use('/', breeds);
 
 module.exports = router;
